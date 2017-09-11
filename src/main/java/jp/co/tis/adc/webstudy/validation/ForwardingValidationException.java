a5ceb5296674592998c3c07599806478e08d4599
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2017-09-11 17:18:00 +0900

    Javadocコメント修正.

--- src/main/java/jp/co/tis/adc/webstudy/validation/ForwardingValidationException.java
+++ src/main/java/jp/co/tis/adc/webstudy/validation/ForwardingValidationException.java
@@ -9,11 +9,20 @@ public class ForwardingValidationException extends ValidationException {
 
     private final String forwardUri;
 
+    /**
+     * コンストラクタ.
+     * @param result バリデーション結果
+     * @param forwardUri フォワード先URI
+     */
     ForwardingValidationException(ValidationResult result, String forwardUri) {
         super(result);
         this.forwardUri = forwardUri;
     }
 
+    /**
+     * フォワード先URIを取得する。
+     * @return フォワード先URI
+     */
     public String getForwardUri() {
         return forwardUri;
     }
--- src/main/java/jp/co/tis/adc/webstudy/validation/ValidationResult.java
+++ src/main/java/jp/co/tis/adc/webstudy/validation/ValidationResult.java
@@ -18,8 +18,14 @@ import java.util.TreeMap;
  */
 public class ValidationResult<T> extends TreeMap<String, Set<String>> {
 
-    private final Set<ConstraintViolation<T>> violations;
+    /** Bean Validation({@link javax.validation.Validator#validate(Object, Class[])})の結果. */
+    private final transient Set<ConstraintViolation<T>> violations;
 
+    /**
+     * コンストラクタ.
+     *
+     * @param violations Bean Validationの結果
+     */
     ValidationResult(Set<ConstraintViolation<T>> violations) {
         this.violations = violations;
         for (ConstraintViolation<T> e : violations) {
@@ -29,20 +35,41 @@ public class ValidationResult<T> extends TreeMap<String, Set<String>> {
         }
     }
 
+    /**
+     * 指定されたプロパティでバリデーションエラーが発生したかどうか判定する。
+     *
+     * @param propertyName プロパティ名
+     * @return エラーである場合、真
+     */
     public boolean isError(String propertyName) {
         return violations.stream().anyMatch(
                 violation -> violation.getPropertyPath().toString().equals(propertyName)
         );
     }
 
+    /**
+     * Bean Validationの結果を取得する。
+     *
+     * @return {@link ConstraintViolation}
+     */
     public Set<ConstraintViolation<T>> getViolations() {
         return violations;
     }
 
+    /**
+     * バリデーションエラーが発生したかどうか判定する。
+     *
+     * @return エラーの場合、真
+     */
     public boolean isError() {
         return !isValid();
     }
 
+    /**
+     * バリデーション結果が妥当であるか（エラーがなかったか）判定する。
+     *
+     * @return 妥当である場合、真
+     */
     public boolean isValid() {
         return violations.isEmpty();
     }
@@ -52,23 +79,55 @@ public class ValidationResult<T> extends TreeMap<String, Set<String>> {
         messages.add(msg);
         return messages;
     }
+
+    /**
+     * 指定されたURIへフォワードする。
+     * {@link ForwardingValidationException}をスローする。
+     * 例外は{@link ValidationFilter}で捕捉される。
+     *
+     * @param forwardUri フォワード先
+     * @throws ForwardingValidationException 必ず送出される
+     */
     public void forward(String forwardUri) {
         throw new ForwardingValidationException(this, forwardUri);
     }
 
+    /**
+     * バリデーションエラーが存在する場合、指定されたURIへフォワードする。
+     *
+     * @param forwardUri フォワード先
+     * @throws ForwardingValidationException 必ず送出される
+     * @see #isError()
+     * @see #forward(String)
+     */
     public void forwardIfInvalid(String forwardUri) {
-        if (!isValid()) {
+        if (isError()) {
             forward(forwardUri);
         }
     }
 
+    /**
+     * バリデーションエラーが存在する場合、エラーとする。
+     *
+     * @throws ValidationException 必ず送出される
+     * @see #isError()
+     * @see #sendError()
+     */
     public void sendErrorIfInvalid() {
-        if (!isValid()) {
+        if (isError()) {
             sendError();
         }
     }
 
-    private void sendError() {
+    /**
+     * エラーを送信する。
+     *
+     * {@link ValidationException}をスローする。
+     * 例外は{@link ValidationFilter}で捕捉される。
+     *
+     * @throws ValidationException 必ず送出される
+     */
+    public void sendError() {
         throw new ValidationException(this);
     }
 }
